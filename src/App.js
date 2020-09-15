63d3e251feac081e7ae5d6256ecb3cb6a823f77f
aryan-tiwari <aryantiwari91000@gmail.com>
2020-09-15 20:22:05 +0530

    Adding and deleting functionality added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Todos from './todos'
+import AddTodo from './addForm'
 class App extends Component {
   state = {
     todos: [
@@ -7,11 +8,28 @@ class App extends Component {
       {id: 2, content: 'play mario cart'}
     ]
   }
+  deleteTodo = (id) =>{
+    const todos = this.state.todos.filter(todo=>{
+      return todo.id !== id
+    })
+    this.setState({
+      // since key and value have the same name, instead of todos: todos
+      todos
+    })
+  }
+  addTodo = (todo) =>{
+    todo.id = Math.random()
+    let todos = [...this.state.todos, todo]
+    this.setState({
+      todos
+    })
+  }
   render(){
     return (
       <div className="todo-app container">
         <h1 className="center blue-text">Todos</h1>
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddTodo addTodo={this.addTodo} />
       </div>
     );
 }
--- src/todos.js
+++ src/todos.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Todos = ({todos})=>{
+const Todos = ({todos, deleteTodo})=>{
     const todoList = todos.length?(
         todos.map(todo=>{
             return(
                 <div className="collection-item" key={todo.id}>
-                    <span>{todo.content}</span>
+                    <span onClick={()=>{deleteTodo(todo.id)}}>{todo.content}</span>
                 </div>
             )
         })
